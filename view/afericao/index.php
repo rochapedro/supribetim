dec7f121a57e22a877b4a85c0dc5bcd542b127bd
rochapedro <pedro.nec@hotmail.com>
2020-11-16 14:34:19 -0300

    estilizações dos selects

--- view/afericao/index.php
+++ view/afericao/index.php
@@ -164,9 +164,9 @@ if (!isset($_SESSION['FERRAM_URL_APP'])){
     
    
  
-  <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script> 
+  <script src="../../js/select/bootstrap-select.min.js"></script> 
   <!-- Importando o js do bootstrap -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script src="../../js/bootstrap/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     
     <script type="text/javascript">
       // Funçao para validar os formulários
